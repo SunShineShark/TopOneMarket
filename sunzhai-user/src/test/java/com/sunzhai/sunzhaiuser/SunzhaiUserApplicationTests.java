78ae56a36099f8384b076c51ff0fbab95439b708
zhaixiangyi <r019741230>
2020-04-09 14:56:50 +0800

    初始化项目并且搭建nacos配置中心

---
+++ sunzhai-user/src/test/java/com/sunzhai/sunzhaiuser/SunzhaiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunzhai.sunzhaiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunzhaiUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
