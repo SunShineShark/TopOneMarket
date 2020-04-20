8b3b19ffd85913590ecb4e50dc344c3001934ec1
zhaixiangyi <r019741230>
2020-04-20 14:51:44 +0800

    修改部分配置

--- cute_market_core/cute_market_auth/src/main/java/com/sunzhai/auth/AuthApplication.java
+++ cute_market_core/cute_market_auth/src/main/java/com/sunzhai/auth/AuthApplication.java
@@ -1,13 +1,29 @@
 package com.sunzhai.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
 
+    @RestController
+    class TestController {
+
+        @Value("${test.test}")
+        private String test;
+
+        @RequestMapping("test")
+        public String test() {
+            return test;
+        }
+    }
 }
