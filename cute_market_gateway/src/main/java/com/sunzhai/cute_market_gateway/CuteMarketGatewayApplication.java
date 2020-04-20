d1e9863cf1cd5c2ea35e67d24c7d58136957e204
sunlixin <153645379@qq.com>
2020-04-20 13:42:36 +0800

    规范项目结构

---
+++ cute_market_gateway/src/main/java/com/sunzhai/cute_market_gateway/CuteMarketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sunzhai.cute_market_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuteMarketGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuteMarketGatewayApplication.class, args);
+    }
+
+}
