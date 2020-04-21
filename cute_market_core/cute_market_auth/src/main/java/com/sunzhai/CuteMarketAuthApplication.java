768921bb649a9a68731040ff3bb08a57582eecf3
sunlixin <153645379@qq.com>
2020-04-21 15:09:58 +0800

    整理项目目录

--- cute_market_core/cute_market_auth/src/main/java/com/sunzhai/CuteMarketAuthApplication.java
+++ cute_market_core/cute_market_auth/src/main/java/com/sunzhai/CuteMarketAuthApplication.java
@@ -1,6 +1,5 @@
 package com.sunzhai;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan(basePackages = {"com.sunzhai.auth.mapper"})
+@MapperScan(basePackages = {"com.sunzhai.mapper"})
 public class CuteMarketAuthApplication {
 
     public static void main(String[] args) {
